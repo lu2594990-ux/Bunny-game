ce4cf9cac1668310b43819f6cd2f6d7e094d49ee
lu2594990-ux <lu2594990@gmail.com>
2026-04-08 02:06:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -522,7 +522,7 @@ jQuery(() => {
                         if (selected[id]) chk.classList.add("on");
                         else chk.classList.remove("on");
                         var cnt = 0;
-                        for (varck in selected) {
+                        for (var ck in selected) {
                             if (selected[ck]) cnt++;
                         }
                         pmsgEl.textContent = "已选 " + cnt + " 条";
